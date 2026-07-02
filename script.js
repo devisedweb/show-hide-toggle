0edf3780488715a03287d1953bea18453be6bb8a
DevisedWeb <contact@devisedweb.com>
2026-07-02 14:51:05 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const showHide = document.getElementById("show-hide");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function () {
+  showHide.classList.toggle("show-hide");
+});
